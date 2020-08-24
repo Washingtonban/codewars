ce20000c5842262c13311211c2085a0bb01f5d0f
Washington Barbosa <34190079+Washingtonban@users.noreply.github.com>
2020-08-24 08:38:24 -0400

    bugfix: fix CountOneBinary method (#3)

--- bitCounting/bit_counting.py
+++ bitCounting/bit_counting.py
@@ -1,16 +1,11 @@
 class CountOneBinary:
 
-    def __init__():
-
-
     def decimal_to_binary(self, decimal:int) -> str:
         '''
         Função que retorna um número binario a partir de um decimal
         :param decimal: Número inteiro positivo
         :return: Número binário em formato de string
         '''
-
-        self.decimal = decimal
 
         if decimal >= 0:
             binary = bin(decimal)
@@ -26,7 +21,4 @@ class CountOneBinary:
         :return: Retorna um inteiro com a contagem de números '1'
         '''
 
-        self.decimal = decimal
-        self.binary = binary
-
-        return decimal_to_binary(decimal).count(binary)
+        return self.decimal_to_binary(decimal).count(binary)
